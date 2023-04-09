6cf30f40c7582ef2046cdbbd0944c0353573bfdf
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-09 18:02:08 -0400

    Updates

---
+++ precache-manifest.d8bfe757aa1304c0713588eb21c264f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf0e8479bb814f09129",
+    "url": "/Sukhvsin2/Some-Basics/static/css/main.203a71ae.chunk.css"
+  },
+  {
+    "revision": "cdf0e8479bb814f09129",
+    "url": "/Sukhvsin2/Some-Basics/static/js/main.c0fd6114.chunk.js"
+  },
+  {
+    "revision": "ee5e0bd941fd70ace70f",
+    "url": "/Sukhvsin2/Some-Basics/static/js/runtime~main.08791901.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/Sukhvsin2/Some-Basics/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/Sukhvsin2/Some-Basics/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Sukhvsin2/Some-Basics/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1971514801da2f8e33940e18c2ac9564",
+    "url": "/Sukhvsin2/Some-Basics/index.html"
+  }
+];
